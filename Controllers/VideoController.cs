049ff514851a5cd817ab940fdd2e853505c22c02
MauricioHdz17 <mauricio.rangel14@hotmail.com>
2020-02-26 11:11:55 -0600

    Segundo commit

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MVCPlantilla.Utilerias;
+
+namespace MvcPlantilla.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Eliminar(int idVideo)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            BaseHelper.ejecutarConsulta("Detele from Video Where idVideo = @idVideo", CommandType.Text, parametros);
+            return View();
+        }
+
+    }
+}
--- Views/Home/Index.aspx
+++ Views/Home/Index.aspx
@@ -10,5 +10,15 @@
     <h1>
     Bienvenido
     </h1>
+     <img src="https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/84827818_10156556900877443_1000852827372781568_n.png?_nc_cat=107&_nc_ohc=C8ujC0zAB_AAX8DbwDp&_nc_ht=scontent.fntr6-1.fna&oh=f08a18a70ed7f36f26af264ba57561e9&oe=5EBCBAC4https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/84827818_10156556900877443_1000852827372781568_n.png?_nc_cat=107&_nc_ohc=C8ujC0zAB_AAX8DbwDp&_nc_ht=scontent.fntr6-1.fna&oh=f08a18a70ed7f36f26af264ba57561e9&oe=5EBCBAC4"/>
+    <hr />
+    <h1>Opciones</h1>
+    <a href="/Video/Index">Ver Videos</a>
+    <hr />
+    <a href="/Video/Index">Agregar Videos</a>
+    <hr />
+    <a href="/Video/Index">Actualizar Videos</a>
+    <hr />
+    <a href="/Video/Eliminar">Eliminar Videos</a>
 </body>
 </html>
